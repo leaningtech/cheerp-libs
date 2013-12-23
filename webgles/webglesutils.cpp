2108a73846dc2b08c2c632f4338323c75aa2b977
Alessandro Pignotti <a.pignotti@sssup.it>
2013-12-23 12:42:55 +0100

    webgles: Add C++ based implementation of BufferForPointer

--- webgles/webglesutils.cpp
+++ webgles/webglesutils.cpp
@@ -19,6 +19,7 @@
  ***************************************************************/
 
 #include "webgles.h"
+#include <duettointrin.h>
 
 client::WebGLRenderingContext* webGLES;
 
@@ -29,4 +30,23 @@ void webGLESInit(const client::String& canvasName)
 	webGLES = reinterpret_cast<client::WebGLRenderingContext*>(canvas->getContext("experimental-webgl"));
 	if (webGLES == NULL)
 		client::window.alert("Sorry, we looked hard, but no sign of WebGL has been found :(");
+}
+
+client::ArrayBufferView* BufferForPointer(const void* ptr)
+{
+	size_t offset=__builtin_duetto_pointer_offset(ptr);
+	//Use an Int8Array as the fake type, it does not matter anyway
+	client::Int8Array* buf=(client::Int8Array*)__builtin_duetto_pointer_base(ptr);
+	if(offset==0)
+		return buf;
+	else
+		return buf->subarray(offset);
+}
+
+client::ArrayBufferView* BufferForPointer(const void* ptr, unsigned int size)
+{
+	size_t offset=__builtin_duetto_pointer_offset(ptr);
+	//Use an Int8Array as the fake type, the actual data type is described by the BYTES_PER_ELEMENT attribute
+	client::Int8Array* buf=(client::Int8Array*)__builtin_duetto_pointer_base(ptr);
+	return buf->subarray(offset, size/buf->get_BYTES_PER_ELEMENT());
 }
