abe06515a7c3c9f6d5279c732a5fa8f05095fee4
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-02-04 09:35:09 +0100

    Clarification of memprof.h descriptions

--- memprof.h
+++ memprof.h
@@ -12,16 +12,16 @@ extern "C"
 	//Create a named tag that will records the allocations/free that will follow
 	__attribute__((cheerp_wasm)) CHEERP_MEMPROF_TAG cheerpMemProfAnnotate(const char *tag_name);
 
-	//Close the tag and report what was create and not yet freed
+	//Close the tag and report what was create and not yet freed (logging the report to the console)
 	__attribute__((cheerp_wasm)) void cheerpMemProfClose(CHEERP_MEMPROF_TAG tag);
 
-	//Report the memory allocated after the tag cration and yet to be freed
+	//Log to the console the memory allocated after the tag creation and yet to be freed
 	__attribute__((cheerp_wasm)) void cheerpMemProfLive(CHEERP_MEMPROF_TAG tag);
 
-	//Report the ammount of memory allocated after the tag creation but not yet freed
+	//Return the ammount of memory allocated after the tag creation and yet to be freed
 	__attribute__((cheerp_wasm)) size_t cheerpMemProfUsed(CHEERP_MEMPROF_TAG tag);
 
-	//Report the total ammount of memory currently allocated by the program 
+	//Return the total ammount of memory currently allocated by the program
 	__attribute__((cheerp_wasm)) size_t cheerpMemProfTotalUsed();
 #ifdef __cplusplus
 }
