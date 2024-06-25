28ef0ebfbf00e4750b91c142fa92de5e5fcaaf2d
Alessandro Pignotti <a.pignotti@sssup.it>
2024-06-25 17:45:01 +0200

    Wasm: Fix memcpy/memset for the valid case of length being 0 and src being NULL

--- wasm/memory.cpp
+++ wasm/memory.cpp
@@ -34,27 +34,32 @@ extern "C"
 			dst8++;
 			src8++;
 		}
-		// Now the dest pointer is aligned
-		// Unroll for 64 bytes at a time
-		while(uintptr_t(src8) <= uintptr_t(srcEnd - 64))
+		// Loop conditions below will underflow for too small
+		// lengths and NULL pointers
+		if(len >= 64)
 		{
-			*((unsigned long long*)dst8) = *((unsigned long long*)src8);
-			*((unsigned long long*)(dst8+8)) = *((unsigned long long*)(src8+8));
-			*((unsigned long long*)(dst8+16)) = *((unsigned long long*)(src8+16));
-			*((unsigned long long*)(dst8+24)) = *((unsigned long long*)(src8+24));
-			*((unsigned long long*)(dst8+32)) = *((unsigned long long*)(src8+32));
-			*((unsigned long long*)(dst8+40)) = *((unsigned long long*)(src8+40));
-			*((unsigned long long*)(dst8+48)) = *((unsigned long long*)(src8+48));
-			*((unsigned long long*)(dst8+56)) = *((unsigned long long*)(src8+56));
-			dst8+=64;
-			src8+=64;
-		}
-		// Loop 8 bytes at a time
-		while(uintptr_t(src8) <= uintptr_t(srcEnd - 8))
-		{
-			*((unsigned long long*)dst8) = *((unsigned long long*)src8);
-			dst8+=8;
-			src8+=8;
+			// Now the dest pointer is aligned
+			// Unroll for 64 bytes at a time
+			while(uintptr_t(src8) <= uintptr_t(srcEnd - 64))
+			{
+				*((unsigned long long*)dst8) = *((unsigned long long*)src8);
+				*((unsigned long long*)(dst8+8)) = *((unsigned long long*)(src8+8));
+				*((unsigned long long*)(dst8+16)) = *((unsigned long long*)(src8+16));
+				*((unsigned long long*)(dst8+24)) = *((unsigned long long*)(src8+24));
+				*((unsigned long long*)(dst8+32)) = *((unsigned long long*)(src8+32));
+				*((unsigned long long*)(dst8+40)) = *((unsigned long long*)(src8+40));
+				*((unsigned long long*)(dst8+48)) = *((unsigned long long*)(src8+48));
+				*((unsigned long long*)(dst8+56)) = *((unsigned long long*)(src8+56));
+				dst8+=64;
+				src8+=64;
+			}
+			// Loop 8 bytes at a time
+			while(uintptr_t(src8) <= uintptr_t(srcEnd - 8))
+			{
+				*((unsigned long long*)dst8) = *((unsigned long long*)src8);
+				dst8+=8;
+				src8+=8;
+			}
 		}
 		// Byte loop to finish the copy
 		while(src8 != srcEnd)
@@ -73,23 +78,26 @@ extern "C"
 		unsigned long long c64 = ((unsigned long long)(c32)<<0) | ((unsigned long long)(c32)<<32);
 		unsigned char* dst8 = (unsigned char*)dst;
 		unsigned char* dstEnd = dst8 + len;
-		while(uintptr_t(dst8) < uintptr_t(dstEnd - 64))
+		if(len >= 64)
 		{
-			*((unsigned long long*)dst8) = c64;
-			*((unsigned long long*)(dst8+8)) = c64;
-			*((unsigned long long*)(dst8+16)) = c64;
-			*((unsigned long long*)(dst8+24)) = c64;
-			*((unsigned long long*)(dst8+32)) = c64;
-			*((unsigned long long*)(dst8+40)) = c64;
-			*((unsigned long long*)(dst8+48)) = c64;
-			*((unsigned long long*)(dst8+56)) = c64;
-			dst8+=64;
-		}
-		// Loop 8 bytes at a time
-		while(uintptr_t(dst8) <= uintptr_t(dstEnd - 8))
-		{
-			*((unsigned long long*)dst8) = c64;
-			dst8+=8;
+			while(uintptr_t(dst8) < uintptr_t(dstEnd - 64))
+			{
+				*((unsigned long long*)dst8) = c64;
+				*((unsigned long long*)(dst8+8)) = c64;
+				*((unsigned long long*)(dst8+16)) = c64;
+				*((unsigned long long*)(dst8+24)) = c64;
+				*((unsigned long long*)(dst8+32)) = c64;
+				*((unsigned long long*)(dst8+40)) = c64;
+				*((unsigned long long*)(dst8+48)) = c64;
+				*((unsigned long long*)(dst8+56)) = c64;
+				dst8+=64;
+			}
+			// Loop 8 bytes at a time
+			while(uintptr_t(dst8) <= uintptr_t(dstEnd - 8))
+			{
+				*((unsigned long long*)dst8) = c64;
+				dst8+=8;
+			}
 		}
 		// Byte loop to finish the copy
 		while(dst8 != dstEnd)
