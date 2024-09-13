86dd899953cc27f689ed330c823d6853f7a98c31
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-09-13 13:48:29 +0200

    remove thread-related functions and thread local usage in the genericjs target

--- system/impl.h
+++ system/impl.h
@@ -3,6 +3,12 @@
 
 #define WEAK __attribute__((weak))
 
+#ifdef __ASMJS__
+#define THREAD_LOCAL _Thread_local
+#else
+#define THREAD_LOCAL
+#endif
+
 namespace sys_internal {
 	double timezone_offset();
 	double real_time_now();
@@ -10,8 +16,8 @@ namespace sys_internal {
 	double cpu_time_now();
 }
 
-extern _Thread_local int tid;
-extern _Thread_local int *clear_child_tid;
+extern THREAD_LOCAL int tid;
+extern THREAD_LOCAL int *clear_child_tid;
 
 extern "C" {
 void __syscall_main_args(int* argc, char*** argv);
