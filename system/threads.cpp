0c49bb0edf12efad314de4e41eb7908b6b30bcef
Mark Peerdeman <mark.peerdeman@leaningtech.com>
2024-11-29 09:49:45 +0100

    Implementation of futex for the main thread
    The main browser thread cannot use the atomic wait operation to sleep, so we have to emulate it using busy waiting.

--- system/threads.cpp
+++ system/threads.cpp
@@ -3,6 +3,7 @@
 #include <cstdarg>
 #include <cstdint>
 #include <sched.h>
+#include <atomic>
 
 #include <cheerpintrin.h>
 #define LEAN_CXX_LIB
@@ -29,7 +30,9 @@ namespace [[cheerp::genericjs]] client {
 
 extern "C" {
 
-long __syscall_futex(int32_t* uaddr, int futex_op, ...)
+std::atomic<uint32_t*> mainThreadWaitAddress = 0;
+
+long __syscall_futex(uint32_t* uaddr, int futex_op, ...)
 {
 	bool isPrivate = futex_op & FUTEX_PRIVATE;
 	bool isRealTime = futex_op & FUTEX_CLOCK_REALTIME;
@@ -57,7 +60,20 @@ long __syscall_futex(int32_t* uaddr, int futex_op, ...)
 			va_start(ap, futex_op);
 			uint32_t val = va_arg(ap, uint32_t);
 			va_end(ap);
-			return __builtin_cheerp_atomic_notify(uaddr, val);
+
+			uint32_t threadsWokenUp = 0;
+			// If the main thread is waiting on this address, handle this case specially.
+			if (uaddr == mainThreadWaitAddress)
+			{
+				// Notify the main thread that it can wake up.
+				mainThreadWaitAddress.store(0);
+				val -= 1;
+				threadsWokenUp = 1;
+				// If there are no other threads to wake up, return here.
+				if (val <= 0)
+					return threadsWokenUp;
+			}
+			return threadsWokenUp + __builtin_cheerp_atomic_notify(uaddr, val);
 		}
 		case FUTEX_WAIT:
 		{
@@ -69,11 +85,33 @@ long __syscall_futex(int32_t* uaddr, int futex_op, ...)
 			int64_t timeout = -1;
 			if (ts != nullptr)
 				timeout = ts->tv_sec * 1000000000 + ts->tv_nsec;
-			uint32_t ret = __builtin_cheerp_atomic_wait(uaddr, val, timeout);
-			if (ret == 1)
-				return EAGAIN;
-			else if (ret == 2)
-				return ETIMEDOUT;
+
+			// If this is the main thread, it's illegal to do a futex wait operation.
+			// Instead, we busy wait while checking a special value.
+			if (tid == 1) // TODO: improve to use actual browser main thread here.
+			{
+				// Manually test the value at uaddr against val. If they do not match, return EAGAIN.
+				if (*uaddr != val)
+					return EAGAIN;
+				mainThreadWaitAddress.store(uaddr);
+				if (*uaddr != val)
+				{
+					mainThreadWaitAddress.store(0);
+					return EAGAIN;
+				}
+				while (mainThreadWaitAddress.load() != 0)
+				{
+				}
+				// TODO : timeout calculation, if elapsed, ETIMEDOUT.
+			}
+			else
+			{
+				uint32_t ret = __builtin_cheerp_atomic_wait(uaddr, val, timeout);
+				if (ret == 1)
+					return EAGAIN; // Value at uaddr did not match val.
+				else if (ret == 2)
+					return ETIMEDOUT;
+			}
 			return 0;
 		}
 		case FUTEX_LOCK_PI:
@@ -199,7 +237,7 @@ bool exit_thread()
 	{
 		// If clear_child_tid is set, write 0 to the address it points to,
 		// and do a FUTEX_WAKE on the address.
-		int *wake_address = clear_child_tid;
+		uint32_t *wake_address = reinterpret_cast<uint32_t*>(clear_child_tid);
 		*clear_child_tid = 0;
 		__syscall_futex(wake_address, FUTEX_WAKE, 1, nullptr, nullptr, 0);
 		return true;
