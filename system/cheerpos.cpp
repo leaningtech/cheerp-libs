ff0beb24fe8e4a6d382891dc642555e61c2bbc5c
Alessandro Pignotti <a.pignotti@sssup.it>
2025-08-27 21:01:29 +0200

    CheerpOS: Avoid another variadic syscall

--- system/cheerpos.cpp
+++ system/cheerpos.cpp
@@ -62,6 +62,17 @@ long __syscall_open(const char* pathname, int flags, ...)
 	return __syscall_open_3(pathname, flags, mode);
 }
 
+long __syscall_openat_4(int dirfd, const char *pathname, int flags, int mode);
+
+long __syscall_openat(int dirfd, const char *pathname, int flags, ...)
+{
+	va_list args;
+	va_start(args, flags);
+	int mode = va_arg(args, int);
+	va_end(args);
+	return __syscall_openat_4(dirfd, pathname, flags, mode);
+}
+
 long __syscall_fcntl64_3(int fd, int op, int arg);
 
 long __syscall_fcntl64(int fd, int op, ...)
